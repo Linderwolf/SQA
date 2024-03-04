093ba8a324270893534b8869434a8001fc93dd72
Linderwolf <peter.linder@ontariotechu.net>
2024-03-03 20:03:12 -0500

    Log batch testing

--- tests/txtFileTests/AvailableGamesTests.bat
+++ tests/txtFileTests/AvailableGamesTests.bat
@@ -5,14 +5,14 @@ pause
 set /a availableGamesWriteFailures=0
 set /a dailyTransactionsWriteFailures=0
 
-for /r %%F in (./txtFileInput/txtAvailableGames/*) do (
+for /r %%F in (.\txtFileInput\txtAvailableGames\*) do (
     echo %%~nF >> report.txt
-    type %%F | ../../Vapour.exe
+    type %%F | ..\..\Vapour.exe
     fc ../AvailableGames.txt ./txtFileOutput/txtAvailableGames/%%~nF.out >> report.txt
     if errorlevel 1 (
         echo Test Failure: %%~nF >> report.txt
         echo Test Output: >> report.txt 
-	echo ../AvailableGames.txt >> report.txt
+rem	echo ../AvailableGames.txt >> report.txt
 	echo ____________________________________
 	echo Expected Output: >> report.txt
 	echo ./txtFileOutput/txtAvailableGames/&&~nF.out >> report.txt
@@ -23,13 +23,13 @@ for /r %%F in (./txtFileInput/txtAvailableGames/*) do (
     if errorlevel 1{
 	echo Test Failure: txtDailyTransactions\AvailableGamesTransactions/%%~nF.out >> report.txt
 	echo Test Output: >> report.txt
-	echo ../DailyTransactions.txt >> report.txt
+rem	echo ../DailyTransactions.txt >> report.txt
 	echo ____________________________________
-	echo Expected Output: >> report.txt
-	echo ./txtFileOutput/txtAvailableGames/&&~nF.out >> report.txt
+	echo Expected Output: >> ./report.txt
+	echo ./txtFileOutput/txtAvailableGames/&&~nF.out >> ./report.txt
 	set /a dailyTransactionsWriteFailures+=1
     )
 )
 echo Write to AvailableGames.txt failures: %availableGamesWriteFailures% >> failureReport.txt
-echo Write to AvailableGames.txt failures: %failures% >> summary.txt
+echo Write to AvailableGames.txt failures: %dailyTransactionsWriteFailures% >> failureReport.txt
 goto main
