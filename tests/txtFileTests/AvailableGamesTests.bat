eb76cff92b08369f1c3fa18d5cfc5fa7bc963fd9
Linderwolf <peter.linder@ontariotechu.net>
2024-03-04 00:10:29 -0500

    refreshFilesFromBackup

--- tests/txtFileTests/AvailableGamesTests.bat
+++ tests/txtFileTests/AvailableGamesTests.bat
@@ -2,16 +2,16 @@
 echo Starting AvailableGames.txt Tests
 pause
 
-rem Copy the AvailableGames.txt, DailyTransactions.txt, etc. files.
 call refreshFilesFromBackup.bat
 
-set /a availableGamesWriteFailures=0		// The number of failures Writing to Available Games
-set /a dailyTransactionsWriteFailures=0		// The number of failures writing to Daily Transactions
+set /a availableGamesWriteFailures=0		&:: The number of test failures found when writing to AvailableGames.txt
+set /a dailyTransactionsWriteFailures=0		&:: rem The number of test failures found when writing to DailyTransactions.txt
 
+rem for every file::
 for /r %%F in (.\txtFileInput\txtAvailableGames\*) do (
-    echo %%~nF >> report.txt
-    type %%F | ..\..\Vapour.exe > testing.txt
-    fc ../../AvailableGames.txt txtFileOutput/txtAvailableGames/%%~nF.out >> report.txt
+    echo %%~nF >> report.txt							  &:: send the file name to report.txt
+    type %%F | ..\..\Vapour.exe >> reports/testing.txt				  &:: pipe the contents to the exe, write output to testing.txt
+    fc AvailableGames.txt txtFileOutput/txtAvailableGames/%%~nF.out >> report.txt &:: compare the testing file to the corresponding input file
     
 if errorlevel 1 (
         echo Test Failure: %%~nF >> report.txt
@@ -19,8 +19,8 @@ if errorlevel 1 (
 	echo ____________________________________
         set /a availableGamesWriteFailures+=1
 )
-
-rem    fc ../DailyTransactions.txt ./txtFileOutput\txtDailyTransactions\AvailableGamesTransactions/%%~nF.out >> report.txt
+)
+rem    fc DailyTransactions.txt ./txtFileOutput\txtDailyTransactions\AvailableGamesTransactions/%%~nF.out >> report.txt
 rem    if errorlevel 1{
 rem	echo Test Failure: txtDailyTransactions\AvailableGamesTransactions/%%~nF.out >> report.txt
 rem	echo Test Output: >> report.txt
@@ -30,7 +30,7 @@ rem	echo Expected Output: >> ./report.txt
 rem	echo ./txtFileOutput/txtAvailableGames/&&~nF.out >> report.txt
 rem	set /a dailyTransactionsWriteFailures+=1
 rem    )
-)
+
 echo Write to AvailableGames.txt failures: %availableGamesWriteFailures% >> failureReport.txt
 rem echo Write to AvailableGames.txt failures: %dailyTransactionsWriteFailures% >> failureReport.txt
 goto main
--- tests/txtFileTests/refreshFilesFromBackup.bat
+++ tests/txtFileTests/refreshFilesFromBackup.bat
@@ -1,8 +1,11 @@
 rem   This file will copy the .txt log files from ../logFileBackup, 
 rem   in order to return to a standard set of data for future tests
-rem   	(Test suite may otherwise fail due to deleting users, for example)
+rem   (Test suite may otherwise fail due to deleting users, for example)
 
-copy ../logFileBackUp/AvailableGames.txt ../AvailableGames.txt
-copy ../logFileBackUp/CurrentUserAccounts.txt ../CurrentUserAccounts.txt
-copy ../logFileBackUp/DailyTransactions.txt ../DailyTransactions.txt
-copy ../logFileBackUp/GameCollection.txt ../GameCollection.txt
+copy ".\logFileBackUp\AvailableGames.txt" "AvailableGames.txt"
+
+copy ".\logFileBackUp\CurrentUserAccounts.txt" "CurrentUserAccounts.txt"
+
+copy ".\logFileBackUp\DailyTransactions.txt" "DailyTransactions.txt"
+
+copy ".\logFileBackUp\GameCollection.txt" "GameCollection.txt"
