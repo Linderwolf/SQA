60dcc060a4d2b2a67451af7fb1ea2ab959b859a2
RusselllN <90791414+RusselllN@users.noreply.github.com>
2024-03-03 21:08:19 -0800

    updated .bat files to use a different temp file called 'testing.txt''

--- tests/create.bat
+++ tests/create.bat
@@ -6,12 +6,12 @@ set /a failures=0
 
 for /r %%F in (.\input\create\*) do (
     echo %%~nF >> report.txt
-    type %%F | ..\Vapour.exe > temp.txt
-    fc temp.txt output/create/%%~nF.out >> report.txt
+    type %%F | ..\Vapour.exe > testing.txt
+    fc testing.txt output/create/%%~nF.out >> report.txt
     if errorlevel 1 (
         echo ==== %%~nF ==== failed >> report.txt
         echo ==== %%~nF ==== failed >> failureReport.txt
-	    fc temp.txt output/create/%%~nF.out >> failureReport.txt
+	    fc testing.txt output/create/%%~nF.out >> failureReport.txt
         set /a failures+=1
     )
 )
--- tests/login.bat
+++ tests/login.bat
@@ -6,12 +6,12 @@ set /a failures=0
 
 for /r %%F in (.\input\login\*) do (
     echo %%~nF >> report.txt
-    type %%F | ..\Vapour.exe > temp.txt
-    fc temp.txt output/login/%%~nF.out >> report.txt
+    type %%F | ..\Vapour.exe > testing.txt
+    fc testing.txt output/login/%%~nF.out >> report.txt
     if errorlevel 1 (
         echo ==== %%~nF ==== failed >> report.txt
         echo ==== %%~nF ==== failed >> failureReport.txt
-	    fc temp.txt output/login/%%~nF.out >> failureReport.txt
+	    fc testing.txt output/login/%%~nF.out >> failureReport.txt
         set /a failures+=1
     )
 )
