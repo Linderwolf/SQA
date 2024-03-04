4a57654a6e3427b6637d17c274eefd89a820c81f
Linderwolf <peter.linder@ontariotechu.net>
2024-03-03 19:45:45 -0500

    exits/logouts to login/logout tests. Working on Log File batch

---
+++ tests/txtFileTests/AvailableGamesTests.bat
@@ -0,0 +1,35 @@
+@echo off
+echo "Starting AvailableGames.txt tests"
+pause
+
+set /a availableGamesWriteFailures=0
+set /a dailyTransactionsWriteFailures=0
+
+for /r %%F in (./txtFileInput/txtAvailableGames/*) do (
+    echo %%~nF >> report.txt
+    type %%F | ../../Vapour.exe
+    fc ../AvailableGames.txt ./txtFileOutput/txtAvailableGames/%%~nF.out >> report.txt
+    if errorlevel 1 (
+        echo Test Failure: %%~nF >> report.txt
+        echo Test Output: >> report.txt 
+	echo ../AvailableGames.txt >> report.txt
+	echo ____________________________________
+	echo Expected Output: >> report.txt
+	echo ./txtFileOutput/txtAvailableGames/&&~nF.out >> report.txt
+        set /a availableGamesWriteFailures+=1
+    )
+
+    fc ../DailyTransactions.txt ./txtFileOutput\txtDailyTransactions\AvailableGamesTransactions/%%~nF.out >> report.txt
+    if errorlevel 1{
+	echo Test Failure: txtDailyTransactions\AvailableGamesTransactions/%%~nF.out >> report.txt
+	echo Test Output: >> report.txt
+	echo ../DailyTransactions.txt >> report.txt
+	echo ____________________________________
+	echo Expected Output: >> report.txt
+	echo ./txtFileOutput/txtAvailableGames/&&~nF.out >> report.txt
+	set /a dailyTransactionsWriteFailures+=1
+    )
+)
+echo Write to AvailableGames.txt failures: %availableGamesWriteFailures% >> failureReport.txt
+echo Write to AvailableGames.txt failures: %failures% >> summary.txt
+goto main
