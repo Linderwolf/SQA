b53a407a46ea79ef735a9754e7aeeeaa8f3e0e0d
RusselllN <90791414+RusselllN@users.noreply.github.com>
2024-03-04 00:03:31 -0800

    updated comments

--- tests/buy.bat
+++ tests/buy.bat
@@ -5,19 +5,19 @@ pause
 set /a failures=0	&:: The number of failures found in the buy tests
 
 rem loop recursively through the input folder
-for /r %%F in (.\input\buy\*) do (			&:: for each buy file
-    echo %%~nF >> report.txt				&:: send the file name to report.txt
-    type %%F | ..\Vapour.exe > testing.txt		&:: pipe the contents to the exe, write output to testing.txt
+for /r %%F in (.\input\buy\*) do (
+    echo %%~nF >> report.txt				            &:: for each buy file send the file name to report.txt
+    type %%F | ..\Vapour.exe > testing.txt		        &:: pipe the contents to the exe, write output to testing.txt
     fc testing.txt output/buy/%%~nF.out >> report.txt	&:: compare the testing file to the corresponding input file
-							&:: record comparison in report.txt    
-rem Format the report   
-if errorlevel 1 (
-	&:: Record the tests that failed.
-        echo ==== %%~nF ==== failed >> report.txt
-        echo ==== %%~nF ==== failed >> failureReport.txt
-	    fc testing.txt output/buy/%%~nF.out >> failureReport.txt
-        set /a failures+=1
-    )
+							                            rem record comparison in report.txt    
+    rem Format the report   
+    if errorlevel 1 (
+        rem Record the tests that failed.
+            echo ==== %%~nF ==== failed >> report.txt
+            echo ==== %%~nF ==== failed >> failureReport.txt
+            fc testing.txt output/buy/%%~nF.out >> failureReport.txt
+            set /a failures+=1
+        )
 )
 rem Send the number of failures to the report file.
 echo Failures in buy: %failures% >> failureReport.txt
