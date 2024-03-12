5ba9f11d58f48a216472cb591cec09d4c2b85102
RusselllN <russell.ngo@ontariotechu.net>
2024-03-11 21:13:09 -0400

    imported functions from HelperFunctions.py

--- Phase_4/TransactionFunctions.py
+++ Phase_4/TransactionFunctions.py
@@ -1,9 +1,15 @@
 # SQA Phase 4
 # Matthew, Ajaane, Peter, Russell
+from HelperFunctions import parse1
+from HelperFunctions import parseRefund
+from HelperFunctions import parseSell
+from HelperFunctions import parseBuy
 
 def logout(transaction):
     return
 def create(transaction):
+    transactionCode,newUser,userType,credit = parse1(transaction)
+    print(newUser)
     return  
 def delete(transaction):
     return  
