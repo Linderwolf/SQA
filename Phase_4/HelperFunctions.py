40de275f5bfec5fa967a4b1997e9590b3be11651
RusselllN <russell.ngo@ontariotechu.net>
2024-03-11 21:12:54 -0400

    made functions for parsing transaction information

--- Phase_4/HelperFunctions.py
+++ Phase_4/HelperFunctions.py
@@ -3,3 +3,21 @@
 
 def getTransactionCode(transaction):
     return transaction[:2]
+
+def parseMost(line): # for create, delete, add credit, and logout
+    transactionCode,newUser,userType,credit = line.split()
+    # print("new username is: " + newUser)
+    # print("user type is: " + userType)
+    return transactionCode,newUser,userType,credit
+
+def parseRefund(line):
+    transactionCode,buyer,seller,credit = line.split()
+    return transactionCode,buyer,seller,credit
+
+def parseSell(line):
+    transactionCode,gameName,seller,gamePrice = line.split()
+    return transactionCode,gameName,seller,gamePrice
+
+def parseBuy(line):
+    transactionCode,gameName,seller,buyer,gamePrice = line.split()
+    return transactionCode,gameName,seller,buyer,gamePrice
