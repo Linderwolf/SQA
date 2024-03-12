d0e7d421b59894dc43b6789c403065ea7f7b5894
RusselllN <russell.ngo@ontariotechu.net>
2024-03-11 21:12:08 -0400

    added comments for transaction requirement stuff

--- Phase_4/SQA_Phase4.py
+++ Phase_4/SQA_Phase4.py
@@ -35,5 +35,36 @@ for transaction in dailyTransactionFile:
         
     
  
+# for create 01, delete 02, add credit 06, and end session / logout 00
+# XX_UUUUUUUUUUUUUUU_TT_CCCCCCCCC
+# X is two digit code
+# U is username (buyer if 2 users in transac)
+# T is user type
+# C is available credit
+# - is a space
 
+# for refund 05
+# XX_UUUUUUUUUUUUUUU_SSSSSSSSSSSSSSS_CCCCCCCCC
+# X is two digit code
+# U is buyer username
+# S is seller username
+# C is refund credit
+# - is a space
+
+# for sell 03
+# XX_IIIIIIIIIIIIIIIIIII_SSSSSSSSSSSSS_PPPPPP
+# X is two digit code
+# I is game name
+# S is seller username
+# P is game price
+# - is a space
+
+# for buy 04
+# XX_IIIIIIIIIIIIIIIIIII_SSSSSSSSSSSSSSS_UUUUUUUUUUUUUU_PPPPPP
+# X is two digit code
+# I is game name
+# S is seller username
+# U is buyer username
+# P is game price
+# - is a space
 
