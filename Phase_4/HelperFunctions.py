451ba97bfcf19cad82c56aa047565e6e67dfc82a
ajaane <ajaane.kanagasabai@ontariotechu.net>
2024-03-13 21:59:36 -0400

    Add buy and refund functionality

--- Phase_4/HelperFunctions.py
+++ Phase_4/HelperFunctions.py
@@ -1,29 +1,89 @@
 # SQA Phase 4
 # Matthew, Ajaane, Peter, Russell
-
+import re
 # Classes
-from UserManager import UserManager
-from GameManager import GameManager
-from CollectionManager import CollectionManager
+import UserManager
+import GameManager
+import CollectionManager
 
 def getTransactionCode(transaction):
     return transaction[:2]
+
+def appendToGameCollection(gameName, seller):
+    with open('GameCollection.txt', 'r') as file:
+        lines = file.readlines()
+    # Modify the content by adding a new line
+    new_line = f"{gameName:<26} {seller:<15}\n"
+    # Find the index of the line with 'END'
+    end_index = lines.index('END')
+    # Insert the new line before the line with 'END'
+    lines.insert(end_index, new_line)
+    file.close()
+
+    # Write the modified content back to the file
+    with open('GameCollection.txt', 'w') as file:
+        file.writelines(lines)
+    file.close()
+    
+def removeGameFromCollection(lineToRemove):
+    with open('GameCollection.txt', 'r') as file:
+        lines = file.readlines()
+
+    # Find the index of the line to remove
+    try:
+        index_to_remove = lines.index(lineToRemove)
+    except ValueError:
+        print(f"Error: Line '{lineToRemove.strip()}' not found in the file.")
+        return
+
+    # Remove the line
+    lines.pop(index_to_remove)
+
+    # Write the modified content back to the file
+    with open('GameCollection.txt', 'w') as file:
+        file.writelines(lines)
+
+# Parsing Methods
 
 def parseMost(line): # for create, delete, add credit, and logout
     newUser, userType, credit = line.split()[1:]
     return newUser, userType, credit
 
 def parseRefund(line):
-    buyer, seller, credit = line.split()[1:]
-    return buyer, seller, credit
+    pattern = re.compile(r'^(\d{2}) (.{25}) (.{15}) (.{15}) (\d+\.\d{2})$')
+    match = pattern.match(line)
+    if match:
+        transactionCode, gameName, buyer, seller, credit = match.groups()
+        gameName = gameName.rstrip()   
+        buyer = buyer.rstrip()       
+        seller = seller.rstrip()                       
+        credit = float(credit) 
+    else:
+        print(f"Invalid transaction format: |{line}|")
+        return None
+    return transactionCode, gameName, buyer, seller, credit
 
 def parseSell(line):
     gameName,seller,gamePrice = line.split()[1:]
     return gameName, seller, gamePrice
 
 def parseBuy(line):
-    gameName, seller, buyer, gamePrice = line.split()[1:]
-    return gameName, seller, buyer, gamePrice
+    pattern = re.compile(r'^(\d{2}) (.{19}) (.{15}) (.{14}) (\d+\.\d{2})$')
+    match = pattern.match(line)
+    if match:
+        transactionCode, gameName, seller, buyer, gamePrice = match.groups()
+        transactionCode = transactionCode
+        gameName = gameName.rstrip()        
+        seller = seller.rstrip()           
+        buyer = buyer.rstrip()              
+        gamePrice = float(gamePrice)        
+    
+    else:
+        print(f"Invalid transaction format: |{line}|")
+        return None
+
+    #print(f'transCode: {transactionCode} game: {gameName} seller: {seller} buyer: {buyer} gamePrice: {gamePrice}')
+    return transactionCode, gameName, seller, buyer, gamePrice
 
 def getUpdatedBalanceString(line, credit):
     updatedBalance = float(line[-9:]) + float(credit)
--- Phase_4/TransactionFunctions.py
+++ Phase_4/TransactionFunctions.py
@@ -3,9 +3,9 @@
 
 # Classes
 
-from UserManager import UserManager
-from GameManager import GameManager
-from CollectionManager import CollectionManager
+import UserManager
+import GameManager
+import CollectionManager
 
 # Helper Functions
 
@@ -13,6 +13,12 @@ from HelperFunctions import parseMost
 from HelperFunctions import parseRefund
 from HelperFunctions import parseSell
 from HelperFunctions import parseBuy
+from HelperFunctions import appendToGameCollection
+from HelperFunctions import removeGameFromCollection
+
+# Specify the file path for CurrentUserAccounts.txt
+userAccountsFilePath = "CurrentUserAccounts.txt"
+availableGameFilePath = "AvailableGames.txt"
 
 def logout(transaction):
     return
@@ -26,8 +32,64 @@ def delete(transaction):
 def sell(transaction):
     return  
 def buy(transaction):
+    # Read user data from the CurrentUserAccounts.txt and store it as a list of User objects in userManager
+    userManager = UserManager.UserFileManager()
+    userManager.readFromFile(userAccountsFilePath)
+    # Read existing games from the AvailableGames.txt file and store it as a list of Game objects in availableGamesManager
+    availableGamesManager = GameManager.AvailableGamesFileManager()
+    availableGamesManager.readAvailableGames(availableGameFilePath)
+    
+    transactionCode, gameName, seller, buyer, gamePrice = parseBuy(transaction)
+
+    # Create User objects for seller and buyer
+    sellerUser = userManager.getUserByUsername(seller)
+    buyerUser = userManager.getUserByUsername(buyer)
+    
+    # Get game price
+    gamePrice = availableGamesManager.getPriceByName(gameName)
+    
+    # Update credits for seller and buyer
+    sellerUser.credit += gamePrice
+    buyerUser.credit -= gamePrice
+    
+    #print(f'buyerUser.credit: {buyerUser.credit}  sellerUser.credit: {sellerUser.credit}')
+    
+    # Write updated user info to CurrentUserAccounts.txt
+    userManager.writeToFile(userAccountsFilePath)
+    # Write to GameCollection.txt 
+    appendToGameCollection(gameName, seller)
     return  
 def refund(transaction):
+    transactionCode, gameName, buyer, seller, refundCredit = parseRefund(transaction)
+    
+    # Get instances of UserFileManager
+    userManager = UserManager.UserFileManager()
+    userManager.readFromFile(userAccountsFilePath)
+    
+    # Retrieve user objects for buyer and seller
+    buyerUser = userManager.getUserByUsername(buyer)
+    sellerUser = userManager.getUserByUsername(seller)
+
+    # Check if both buyer and seller exist
+    if buyerUser is not None and sellerUser is not None:
+        # Calculate the updated balances
+        newBuyerBalance = buyerUser.credit + float(refundCredit)
+        newSellerBalance = sellerUser.credit - float(refundCredit)
+
+        # Update user balances
+        userManager.updateUsercredit(buyer, newBuyerBalance)
+        userManager.updateUsercredit(seller, newSellerBalance)
+        
+        # Write updated user info to CurrentUserAccounts.txt
+        userManager.writeToFile(userAccountsFilePath)
+        #print(f"Refund successful. Buyer: {buyer}, Seller: {seller}, Refund Credit: {refundCredit}")
+        
+        # Remove game from buyer's game collection
+        lineToRemove = f"{gameName:<26} {buyer:<15}\n"
+        removeGameFromCollection(lineToRemove)
+        
+    else:
+        print(f"Error: Buyer ({buyer}) or Seller ({seller}) not found.")
     return   
 def addCredit(transaction):
     user, userType, credit = parseMost(transaction)
